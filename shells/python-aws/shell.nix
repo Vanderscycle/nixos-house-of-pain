00e07bfb3614838c5c9853fb6a991c13d728cac2
vanderscycle <henri-vandersleyen@protonmail.com>
2024-01-26 14:37:59 -0800

    updated the files

--- shells/python-aws/shell.nix
+++ shells/python-aws/shell.nix
@@ -1,11 +1,11 @@
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
-  buildInputs = [
-    pkgs.python310
-    pkgs.python310Packages.pyyaml
-    pkgs.python310Packages.boto3
-    pkgs.awscli
+  buildInputs = with pkgs; [
+    python311
+    python311Packages.pyyaml
+    python311Packages.boto3
+    awscli
   ];
 
   shellHook = ''
